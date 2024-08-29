0d2acdb4e2f7889669a2c64d3d986950b6dc0329
javedmughal <javedmughal609@gmail.com>
2024-08-29 15:30:45 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# free_hd_wallpapers_app
+ A project which implement free hd wallpapers for your mobile screen.
---
+++ android/app/src/main/kotlin/com/jd_studio/free_hd_wallpapers/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jd_studio.free_hd_wallpapers
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
